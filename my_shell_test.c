f9e8630a42f6b1c84b15bd130c004694df5b2f14
Pluto <1799718517@qq.com>
2024-03-17 23:51:00 +0800

    4

--- my_shell_test.c
+++ my_shell_test.c
@@ -4,66 +4,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-#define PATHNAME_MAX 200
-#define COMMAND_MAX  200
-#define ARG_MAX      10     
 
-void read_command();
-int main(int argc,char*argv[])
+#define PATHNAME_MAX  200
+#define COMMAND_MAX   200
+#define ARG_MAX       10
+
+void change_dir(char *argv[]);
+int main()
 {
     while(1)
     {
         char *cwdpath=(char *)malloc(sizeof(char)*PATHNAME_MAX);
         getcwd(cwdpath,PATHNAME_MAX);
         printf("%s\n",cwdpath);
-        read_command();
-
+        char *input=readline("$ ");
+        if((input)!=NULL) 
+        {
+            add_history(input);
+        }
+        char *tem_command=strdup(input);
+        int i=0;
+        int argc=0;
+        if(input==NULL)
+        {
+            return 0;
+        }
+        //求出argc
+        char *temp=strtok(tem_command," ");
+        while(temp!=NULL)
+        {
+            argc++;
+            temp=strtok(tem_command," ");
+        }
+        //为参数分配空间
+        char **argv=(char **)malloc(sizeof(char *)*argc);
+        for(int i=0;i<argc;i++)
+        {
+            argv[i]=(char *)malloc(sizeof(char)*ARG_MAX);
+        }
+        //存入参数
+        char *temp2=strtok(input," ");
+        while(temp2!=NULL)
+        {
+            argv[i++]=temp2;
+            temp2=strtok(input," ");
+        }
+        if(strcmp(argv[0],"cd")==0)
+        {
+            change_dir(argv);
+        }
+        if(strcmp(argv[0],"exit")==0)
+        {
+            break;
+        }
     }
     return 0;
 }
-void read_command()
+void change_dir(char *argv[])
 {
-    char *command=(char *)malloc(sizeof(char)*COMMAND_MAX);
-    fgets(command,200,stdin);
-    char *tem_command;
-    int i=0;
-    int argc=0;
-    //求出argc
-    char *temp=strtok(tem_command,' ');
-    while(temp!=NULL)
-    {
-        argc++;
-        temp=strtok(tem_command,' ');
-    }
-    //为参数分配空间
-    char **argv=(char **)malloc(sizeof(char *)*argc);
-    for(int i=0;i<argc;i++)
-    {
-        argv[i]=(char *)malloc(sizeof(char)*ARG_MAX);
-    }
-    //存入参数
-    char *temp=strtok(command,' ');
-    while(temp!=NULL)
-    {
-        argv[i++]=temp;
-        temp=strtok(command,' ');
-    }
-    //判断参数
-    int count=0;
-    for(int i=0;i<argc;i++)
-    {
-        if(argv[i]==">"||argv[i]=="<"||argv[i]==">>"||argv[i]=="<<"||argv[i]=="|"||argv[i]=="|")
-        {
-            count=1;
-        }
-    }
-    if(count==0)
-    {
 
-    }
-    else
-    {
-
-    }
 }
